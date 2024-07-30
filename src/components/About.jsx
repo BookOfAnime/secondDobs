9176e378cb56483c6e9cc1c26be918807e271cb9
Ephriam Kassa <ephriam123@gmail.com>
2024-07-29 21:36:13 -0700

    dasdasd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import './About.css';
+import "./About.css";
 
 const About = () => {
   const aboutRef = useRef(null);
@@ -39,16 +39,16 @@ const About = () => {
         <span className="animate-on-scroll">W</span>
         <span className="animate-on-scroll">h</span>
         <span className="animate-on-scroll">o</span>
-        <span className="animate-on-scroll">   </span>
+        <span className="animate-on-scroll">&nbsp;</span>
         <span className="animate-on-scroll">i</span>
         <span className="animate-on-scroll">s</span>
-        <span className="animate-on-scroll"> </span>
+        <span className="animate-on-scroll">&nbsp;</span>
         <span className="animate-on-scroll">$</span>
         <span className="animate-on-scroll">D</span>
         <span className="animate-on-scroll">O</span>
         <span className="animate-on-scroll">B</span>
         <span className="animate-on-scroll">S</span>
-        <span className="animate-on-scroll">?</span>
+        <span className="animate-on-scroll last">?</span>
       </h2>
       <div className="about-content">
         <div className="about-image animate-on-scroll">
@@ -60,11 +60,29 @@ const About = () => {
         </div>
         <div className="about-text">
           <p className="animate-on-scroll">
-            <span className="highlight animate-on-scroll">DOBs</span> is a <span className="highlight animate-on-scroll">legendary</span> character within the Digital world of Solana,
-            known as the <span className="highlight animate-on-scroll">Guardian of Trust</span>. He embodies <span className="highlight animate-on-scroll">integrity</span>, <span className="highlight animate-on-scroll">transparency</span>, and <span className="highlight animate-on-scroll">community protection</span>, ensuring fairness in all transactions.
+            <span className="highlight animate-on-scroll">DOBs</span> is a{" "}
+            <span className="highlight animate-on-scroll">legendary</span>{" "}
+            character within the Digital world of Solana, known as the{" "}
+            <span className="highlight animate-on-scroll">
+              Guardian of Trust
+            </span>
+            . He embodies{" "}
+            <span className="highlight animate-on-scroll">integrity</span>,{" "}
+            <span className="highlight animate-on-scroll">transparency</span>,
+            and{" "}
+            <span className="highlight animate-on-scroll">
+              community protection
+            </span>
+            , ensuring fairness in all transactions.
           </p>
           <p className="animate-on-scroll">
-            DOBs' unwavering <span className="highlight animate-on-scroll">dedication</span> thrives and conflicts are <span className="highlight animate-on-scroll">peacefully resolved</span>, making him a referred figure in Solana's history.
+            DOBs' unwavering{" "}
+            <span className="highlight animate-on-scroll">dedication</span>{" "}
+            thrives and conflicts are{" "}
+            <span className="highlight animate-on-scroll">
+              peacefully resolved
+            </span>
+            , making him a referred figure in Solana's history.
           </p>
         </div>
       </div>
